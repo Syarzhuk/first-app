d9398c0baeaf27eb36406976cdec45cfc288625a
Sergei Stanislauchyk <sergeistanislauchyk@air-sergei>
2015-11-28 12:35:17 +0300

    Initial Commit

---
+++ SooperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SooperCool
+//
+//  Created by Sergei Stanislauchyk on 28.11.15.
+//  Copyright © 2015 Rub of the Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
